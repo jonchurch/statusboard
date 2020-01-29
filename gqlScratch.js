8e7912aff20f02f2f7e1b8b309b2d20292bc618c
Jon Church <me@jonchurch.com>
2020-01-29 14:51:30 -0500

    run request for orgs in parallel

--- gqlScratch.js
+++ gqlScratch.js
@@ -21,7 +21,7 @@ organization(login: $org) {
   return data.organization.repositories.totalCount
 }
 
-async function getIssuesForOrg (org, repoCount) {
+async function requestIssuesForOrg (org, repoCount) {
   const query = `query($org: String!, $repoCount: Int!) {
   organization(login: $org) {
      repositories(first: $repoCount) {
@@ -110,21 +110,27 @@ function findReposWithMoreIssuesToGet (repos, owner) {
     return { name, cursor, owner }
   })
 }
-async function run (orgName) {
+async function getOrgIssues (orgName) {
+  console.log('\n')
+  console.log('==========Running ', orgName)
+  console.log('\n')
   try {
     const repoCount = await getRepoCountForOrg(orgName)
     // console.log({ repoCount })
     // once I know how many repos are in an org, I can more efficiently grab exactly how many repos I need on the first pass
-    const repos = await getIssuesForOrg(orgName, repoCount)
+    // this matters only slightly, if you tell GH to get X repos for an org (can be like 1k I believe) you get charged X rate limit credits
+    // since it's a simple query, no harm in doing it first
+    const repos = await requestIssuesForOrg(orgName, repoCount)
     console.log(repos)
     // great, now I have repos and _some_ of their issues
     // For some repos, most prolly, this is all we need to do, we're done because we've retrieved all the issues
     const reposWithMoreIssuesToGet = findReposWithMoreIssuesToGet(repos, orgName)
-    // console.log({ reposWithMoreIssuesToGet })
-    const remaining = await makeRequestForRemaining(reposWithMoreIssuesToGet)
-    // console.log(remaining)
-		Object.keys(remaining).forEach(key => console.log(`${key}:${remaining[key].issues.edges.length}`))
-		
+    console.log({ reposWithMoreIssuesToGet })
+    if (reposWithMoreIssuesToGet.length) {
+      const remaining = await makeRequestForRemaining(reposWithMoreIssuesToGet)
+      console.log(remaining)
+      Object.keys(remaining).forEach(key => console.log(`${key}:${remaining[key].issues.edges.length}`))
+    }
   } catch (err) {
     if (err.length) {
       err.forEach(console.log)
@@ -134,4 +140,11 @@ async function run (orgName) {
   }
 }
 
-run('expressjs')
+async function run () {
+  const orgs = ['pillarjs', 'expressjs', 'jshttp']
+  // for (const org of orgs) {
+  //   await getOrgIssues(org)
+	// }
+		orgs.forEach(getOrgIssues)
+}
+run()
